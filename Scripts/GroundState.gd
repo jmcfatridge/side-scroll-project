c4d2cbf367420ff4393865cf6eb8bfa8024a9e00
Joseph McFatridge <josephdanielmcfatridge@gmail.com>
2025-05-15 13:56:03 -0500

    Created the scripts and logic for the attack animations to trigger and switch back to the move state

--- Scripts/GroundState.gd
+++ Scripts/GroundState.gd
@@ -5,7 +5,8 @@ class_name GroundState
 @export var jump_velocity : float = -160
 @export var air_state : State
 @export var jump_animation : String = "jump"
-
+@export var attack_state : State
+@export var attack_animation : String = "attack_1"
 
 func state_process(delta):
 	if(!character.is_on_floor()):
@@ -14,8 +15,14 @@ func state_process(delta):
 func state_input(event : InputEvent):
 	if (event.is_action_pressed("jump")):
 		jump()
+	if (event.is_action_pressed("attack")):
+		attack()
 
 func jump():
 	character.velocity.y = jump_velocity
 	next_state = air_state
 	playback.travel(jump_animation)
+
+func attack():
+	next_state = attack_state
+	playback.travel(attack_animation)
